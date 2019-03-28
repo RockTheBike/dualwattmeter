dcccfaf479331bf6ddd1e9f7b540a7b733150428
jake waters <jerkey@g-cipher.net>
2019-03-27 18:11:25 -0700

    it compiles

--- dualwattmeter.ino
+++ dualwattmeter.ino
@@ -1,79 +1,79 @@
-/*TED MODEL: 1001 MTU
-100-130V~, 1W
-50/60Hz
-J4 is an 8-pin connector
-2 Vcc  +side of C2 and C5 5.00v
-3 GND  -side of C2 and C5
-7 weird pulses, saved a logic capture, 0.43ms hi,50us lo,73us hi, 13us lo
-
-pin 21 (4th from last) of U1 pulses low every 0.00118 watt-hours for 4uS
-pin 22 (3rd from last) of U1 will also be low if direction is the other way
-*/
-
-#define VERSIONSTR "giant whatwatt display with YTD watt-hour numeric display"
+#include <Adafruit_NeoPixel.h>
+#define VERSIONSTR "dualwattmeter standalone box"
 #define BAUDRATE 57600
-#define PRINTHEADER "millis()\t\tlastPrintDisplay\twatthours\t\twattage" // a legend to the printed header
+#define PRINTHEADER "millis()\t\tlastPrintDisplay\t\twattage0\t\twattage1" // a legend to the printed header
 
 #define PRINTDISPLAYRATE 1000 // how many milliseconds between running printDisplay
-#define LEDDISPLAYRATE 1000 // how many milliseconds between running updateDisplay
+#define UPDATEDISPLAYRATE 1000 // how many milliseconds between running updateDisplays
 
-#define ENERGYPULSE     0.00118 // this many watt-hours have been used
-volatile float wattHours; // stores total counted energy
-float wattage = 0; // what is our present measured wattage
-unsigned long lastWattCalcTime = 0; // when's the last time we calculated wattage
+#define VOLTPIN A0 // Voltage Sensor Pin
+#define AMPSPIN0 A3 // Current Sensor Pin
+#define AMPSPIN1 A2 // Current Sensor Pin
+#define NOISYZERO 0.5  // assume any smaller measurement should be 0
+#define OVERSAMPLING 25.0 // analog oversampling
+#define VOLTCOEFF 13.36  // larger number interprets as lower voltage
+#define AMPCOEFF 13.05  // PLUSOUT = OUTPUT, PLUSRAIL = PEDAL INPUT
+#define AMPOFFSET 118.0 // when current sensor is at 0 amps this is the ADC value
+float voltage, current0, current1, wattage0, wattage1;
+
 unsigned long lastPrintDisplay = 0; // when's the last time we did printDisplay
-unsigned long lastLEDDisplay = 0; // when's the last time we did updateDisplay
+unsigned long lastUpdateDisplays = 0; // when's the last time we did updateDisplays
 unsigned long backupTimer = 0;
-float lastWattCalcWattHours = 0.0; // what our energy count was last time we calculated wattage
-#define WATTCALCTIME    100 // how many milliseconds between recalculating wattage
 
-#include <EEPROM.h>
-#define WATTHOURS_EEPROM_ADDRESS 20 // long-term memory in EEPROM
-#define BACKUP_INTERVAL 30000 // how often to write our energy to the EEPROM
-#define WATTHOUR_RESET_PIN      6
 #define POWER_STRIP_PIN         7
 #define POWER_STRIP_PIXELS      40 // number of pixels in whatwatt power column
-#define WATTHOUR_DISPLAY_PIN    4
-#define WATTHOUR_DISPLAY_PIXELS (8*28) // actually 27 wide but leftmost doesn't exist
+#define DISPLAY0_PIN    4
+#define DISPLAY1_PIN    5
+#define DISPLAY_PIXELS (8*28) // actually 27 wide but leftmost doesn't exist
 // bottom right is first pixel, goes up 8, left 1, down 8, left 1...
 // https://www.aliexpress.com/item/8-32-Pixel/32225275406.html
-#define ENERGYPULSE_PIN         8 // pin 8 = PB0 aka PCINT0_vect
-#include <Adafruit_NeoPixel.h>
 #include "font1.h"
 uint32_t fontColor = Adafruit_NeoPixel::Color(64,64,25);
 uint32_t backgroundColor = Adafruit_NeoPixel::Color(0,0,1);
-Adafruit_NeoPixel wattHourDisplay = Adafruit_NeoPixel(WATTHOUR_DISPLAY_PIXELS, WATTHOUR_DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel powerStrip = Adafruit_NeoPixel(POWER_STRIP_PIXELS, POWER_STRIP_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel display0 = Adafruit_NeoPixel(DISPLAY_PIXELS, DISPLAY0_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel display1 = Adafruit_NeoPixel(DISPLAY_PIXELS, DISPLAY1_PIN, NEO_GRB + NEO_KHZ800);
 
 // scale the logarithmic displays
 #define MIN_POWER 1
 #define MAX_POWER 50000
 
-ISR(PCINT0_vect) { // fire an interrupt when PB0 changes state
-  if (PINB & _BV(PB0)) wattHours += ENERGYPULSE; // this pulse means ENERGYPULSE energy was used
-}
-
 void setup() {
   Serial.begin(BAUDRATE);
   Serial.println(VERSIONSTR);
-  digitalWrite( WATTHOUR_RESET_PIN, HIGH );  // we want to read with pullup enabled
-  load_watthours(); // read wattHours from EEPROM
   Serial.println(PRINTHEADER);
-  PCICR |= _BV(PCIE0); //  Pin Change Interrupt enable on PCINT0
-  PCMSK0 |= _BV(PCINT0); // enable PCINT0 for PB0
-  wattHourDisplay.begin();
-  wattHourDisplay.show();
-  powerStrip.begin(); // Initialize all pixels to 'off'
-  powerStrip.show(); // Initialize all pixels to 'off'
+  display0.begin();
+  display0.show();
+  display1.begin();
+  display1.show();
 }
 
 void loop() {
-  updateDisplay();
+  updateDisplays();
   printDisplay();
-  updateWattage();
+  getVoltages();
   // wattage = (millis()/100)%10000; // for testing powerStrip
-  updatePowerStrip();
-  storeEnergy();
+  // updatePowerStrip();
+}
+
+void getVoltages(){
+  voltage = average(analogRead(VOLTPIN) / VOLTCOEFF, voltage);
+
+  uint32_t ampsRaw = 0; // reset adder
+  for(int j = 0; j < OVERSAMPLING; j++) ampsRaw += analogRead(AMPSPIN0) - AMPOFFSET;
+  current0 = ((float)ampsRaw / OVERSAMPLING) / AMPCOEFF;  // PLUSOUT = OUTPUT, PLUSRAIL = PEDAL INPUT
+  if( current0 < NOISYZERO ) current0 = 0; // we assume anything near or below zero is a reading error
+  wattage0 = voltage * current0;
+
+  ampsRaw = 0; // reset adder
+  for(int j = 0; j < OVERSAMPLING; j++) ampsRaw += analogRead(AMPSPIN1) - AMPOFFSET;
+  current1 = ((float)ampsRaw / OVERSAMPLING) / AMPCOEFF;  // PLUSOUT = OUTPUT, PLUSRAIL = PEDAL INPUT
+  if( current1 < NOISYZERO ) current1 = 0; // we assume anything near or below zero is a reading error
+  wattage1 = voltage * current1;
+}
+
+float average(float val, float avg){
+  if(avg == 0) avg = val;
+  return (val + (avg * (OVERSAMPLING - 1))) / OVERSAMPLING;
 }
 
 void printDisplay() {
@@ -82,30 +82,22 @@ void printDisplay() {
     Serial.print("\t\t\t");
     Serial.print(millis() - lastPrintDisplay);
     Serial.print("\t\t\t");
-    Serial.print(wattHours);
+    Serial.println(wattage0);
     Serial.print("\t\t\t");
-    Serial.println(wattage);
+    Serial.println(wattage1);
     lastPrintDisplay = millis();
   }
 }
 
-void updateWattage() {
-  if (millis() - lastWattCalcTime > WATTCALCTIME) {
-    wattage = (wattHours - lastWattCalcWattHours) * 3600000 / (millis() - lastWattCalcTime); // accuracy affected by millis() errors caused by *.show()
-    lastWattCalcWattHours = wattHours;
-    lastWattCalcTime = millis();
-  }
-}
-
-void updatePowerStrip(){
+void updatePowerStrip(const Adafruit_NeoPixel& strip, int wattage){
   float ledstolight;
   ledstolight = logPowerRamp(wattage);
   if( ledstolight > POWER_STRIP_PIXELS ) ledstolight=POWER_STRIP_PIXELS;
   unsigned char hue = ledstolight/POWER_STRIP_PIXELS * 170.0;
-  uint32_t color = Wheel(powerStrip, hue<1?1:hue);
+  uint32_t color = Wheel(strip, hue<1?1:hue);
   static const uint32_t dark = Adafruit_NeoPixel::Color(0,0,0);
-  doFractionalRamp(powerStrip, 0, POWER_STRIP_PIXELS, ledstolight, color, dark);
-  powerStrip.show();
+  doFractionalRamp(strip, 0, POWER_STRIP_PIXELS, ledstolight, color, dark);
+  strip.show();
 }
 
 float logPowerRamp( float p ) {
@@ -164,17 +156,18 @@ uint32_t weighted_average_of_colors( uint32_t colorA, uint32_t colorB, float fra
     BA*fraction + BB*(1-fraction) );
 }
 
-void updateDisplay() {
-  if (millis() - lastLEDDisplay > LEDDISPLAYRATE) {
+void updateDisplays() {
+  if (millis() - lastUpdateDisplays > UPDATEDISPLAYRATE) {
     char buf[]="    "; // stores the number we're going to display
-    //sprintf(buf,"%4d",millis()/100);// for testing display
-    sprintf(buf,"%4d",(int)(wattHours / 29));
-    writeWattHourDisplay(buf);
-    lastLEDDisplay = millis();
+    sprintf(buf,"%4d",millis()/100);// for testing display
+    //sprintf(buf,"%4d",(int)(wattHours / 29));
+    writeDisplay(display0, buf);
+    writeDisplay(display1, buf);
+    lastUpdateDisplays = millis();
   }
 }
 
-void writeWattHourDisplay(char* text) {
+void writeDisplay(const Adafruit_NeoPixel& strip, char* text) {
 #define DISPLAY_CHARS   4 // number of characters in display
 #define FONT_W 7 // width of font
 #define FONT_H 8 // height of font
@@ -192,65 +185,15 @@ void writeWattHourDisplay(char* text) {
       for (int fontYIndex=0; fontYIndex<FONT_H; fontYIndex++) {
         uint32_t pixelColor = buffer[fontYIndex][FONT_W-1-fontXIndex]=='0' ? fontColor : backgroundColor; // here is where the magic happens
         if ((FONT_W*(DISPLAY_CHARS-1-textIndex) + fontXIndex) & 1) { // odd columns are top-to-bottom
-          wattHourDisplay.setPixelColor((FONT_H*FONT_W)*(DISPLAY_CHARS-1-textIndex) + fontXIndex*FONT_H +           fontYIndex ,pixelColor);
+          strip.setPixelColor((FONT_H*FONT_W)*(DISPLAY_CHARS-1-textIndex) + fontXIndex*FONT_H +           fontYIndex ,pixelColor);
         } else { // even columns are bottom-to-top
-          wattHourDisplay.setPixelColor((FONT_H*FONT_W)*(DISPLAY_CHARS-1-textIndex) + fontXIndex*FONT_H + (FONT_H-1-fontYIndex),pixelColor);
+          strip.setPixelColor((FONT_H*FONT_W)*(DISPLAY_CHARS-1-textIndex) + fontXIndex*FONT_H + (FONT_H-1-fontYIndex),pixelColor);
         }
       }
     }
   }
-  wattHourDisplay.setPixelColor((FONT_W-1)*FONT_H+0,fontColor); // light up the decimal point
-  wattHourDisplay.setPixelColor((FONT_W  )*FONT_H+7,backgroundColor); // keep decimal point visible
-  wattHourDisplay.setPixelColor((FONT_W-2)*FONT_H+7,backgroundColor); // keep decimal point visible
-  wattHourDisplay.show(); // send the update out to the LEDs
-}
-
-void storeEnergy() {
-  if( digitalRead( WATTHOUR_RESET_PIN ) ) {  // reset switch is not resetting
-    if( millis() - backupTimer >= BACKUP_INTERVAL ) {  // store wattHours into eeprom
-      store_watthours();
-      backupTimer = millis();
-    }
-  } else {  // reset switch is resetting
-    reset_watthours();
-    backupTimer = millis();
-  }
-}
-
-union float_and_byte {
-  float f;
-  unsigned char bs[sizeof(float)];
-} fab;
-
-void store_watthours() {
-  Serial.println( "Storing wattHours." );
-  Serial.println(PRINTHEADER);
-  fab.f = wattHours;
-  for(int i=0; i<sizeof(float); i++ )
-    EEPROM.write( WATTHOURS_EEPROM_ADDRESS+i, fab.bs[i] );
-}
-
-void load_watthours() {
-  Serial.print( "Loading watthours bytes 0x" );
-  bool blank = true;
-  for(int i=0; i<sizeof(float); i++ ) {
-    fab.bs[i] = EEPROM.read( WATTHOURS_EEPROM_ADDRESS+i );
-    Serial.print( fab.bs[i], HEX );
-    if( blank && fab.bs[i] != 0xff )  blank = false;
-  }
-  wattHours = blank ? 0 : fab.f;
-  Serial.print( ", so wattHours is " );
-  Serial.print( wattHours );
-  Serial.println( "." );
-}
-
-void reset_watthours() {
-  load_watthours();
-  if (wattHours != 0.0) {
-    Serial.println("reset_watthours(): store_watthours()");
-    wattHours = 0;
-    store_watthours();
-  } else {
-    Serial.println("reset_watthours(): watthours already reset!");
-  }
+  strip.setPixelColor((FONT_W-1)*FONT_H+0,fontColor); // light up the decimal point
+  strip.setPixelColor((FONT_W  )*FONT_H+7,backgroundColor); // keep decimal point visible
+  strip.setPixelColor((FONT_W-2)*FONT_H+7,backgroundColor); // keep decimal point visible
+  strip.show(); // send the update out to the LEDs
 }
